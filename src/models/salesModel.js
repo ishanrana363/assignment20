4b47bbf362e45a5aaaf3e7ee00173cca2f14f0f1
ishanrana363 <ishanrana363@gmail.com>
2024-01-08 20:24:08 +0600

    basick file folder structure and database connect done

---
+++ src/models/salesModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const salesSchema = new mongoose.Schema({
+    product : {
+        type : String,
+        required : true
+    },
+    quantity : {
+        type : Number,
+        required : true
+    },
+    date : {
+        type: Date,
+        required: true,
+    },
+    department : {
+        type : String,
+        required : true
+    }
+},{versionKey:false});
+
+const salesModel = mongoose.model("sales",salesSchema);
+
+module.exports = salesModel;
